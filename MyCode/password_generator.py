9ebe23afcdb964edbc1b6ccc13027ae778b17ea6
Sophia <sophia.miller.0419@gmail.com>
2024-07-03 18:46:41 +0800

    Create password_generator.py

---
+++ MyCode/password_generator.py
@@ -0,0 +1,10 @@
+# password_generator.py
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+print(generate_password(12))
