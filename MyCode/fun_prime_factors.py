65d02c8e1aed32e4a016f4afa37a51aafe4bc49f
Sophia <sophia.miller.0419@gmail.com>
2024-07-07 16:32:55 +0800

    Create fun_prime_factors.py

---
+++ MyCode/fun_prime_factors.py
@@ -0,0 +1,15 @@
+# fun_prime_factors.py
+def fun_prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+print(fun_prime_factors(56))
