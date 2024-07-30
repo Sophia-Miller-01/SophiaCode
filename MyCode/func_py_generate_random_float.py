b80be11452b3a1040322abce87faf61731cf8a6c
Sophia <sophia.miller.0419@gmail.com>
2024-07-30 18:40:03 +0800

    Create func_py_generate_random_float.py

---
+++ MyCode/func_py_generate_random_float.py
@@ -0,0 +1,7 @@
+# func_py_generate_random_float.py
+import random
+
+def func_py_generate_random_float(start, end):
+    return random.uniform(start, end)
+
+print(func_py_generate_random_float(1.5, 10.5))
