074b3acc3a12643db92d618ad17199c7f80d95d5
Sophia <sophia.miller.0419@gmail.com>
2024-08-19 19:00:57 +0800

    Create func_py_calculate_right_circular_cone_volume.py

---
+++ MyCode/func_py_calculate_right_circular_cone_volume.py
@@ -0,0 +1,7 @@
+# func_py_calculate_right_circular_cone_volume.py
+import math
+
+def func_py_calculate_right_circular_cone_volume(radius, height):
+    return (1/3) * math.pi * radius**2 * height
+
+print(func_py_calculate_right_circular_cone_volume(3, 7))
