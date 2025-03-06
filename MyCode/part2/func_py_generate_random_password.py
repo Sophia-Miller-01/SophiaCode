e32029e088d1d45582d6c0948558d3d7beb4a8c0
Sophia <sophia.miller.0419@gmail.com>
2025-03-06 16:39:52 +0800

    Create func_py_generate_random_password.py

---
+++ MyCode/part2/func_py_generate_random_password.py
@@ -0,0 +1,9 @@
+# func_py_generate_random_password.py
+import random
+import string
+
+def func_py_generate_random_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print(func_py_generate_random_password(16))
